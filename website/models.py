888cbfacf44d77857918af23f1125155e5e090a7
Joshua Leong <90120658+JoshuaLSE@users.noreply.github.com>
2022-11-14 21:53:59 +0800

    Added SQL Handler in models.py

---
+++ website/models.py
@@ -0,0 +1,73 @@
+# SQLAlchemy-related Imports
+from sqlalchemy import *
+from sqlalchemy.orm import sessionmaker
+import sqlalchemy as db
+from sqlalchemy.ext.declarative import declarative_base
+
+# MySQL-related Imports
+from typing import List, Dict
+import mysql.connector
+
+
+def test_table() -> List[Dict]:
+    config = {
+        "user": "root",
+        "password": "root",
+        "host": "db",
+        "port": "3306",
+        "database": "02db",
+    }
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM test_table")
+    results = [{name: color} for (name, color) in cursor]
+    cursor.close()
+    connection.close()
+
+    return results
+
+
+class ImageSQL:
+    Base = declarative_base()
+
+    class Image(Base):
+        __tablename__ = 'Image'
+        ID = db.Column(db.INTEGER, primary_key=True)
+        image_url = db.Column(db.VARCHAR(255))
+        caption = db.Column(db.VARCHAR(255))
+
+    def __init__(self, username, password, ip, port, table_name):
+        inner_engine = db.create_engine(
+            "mysql+pymysql://%s:%s@%s:%s/%s" % (username, password, ip, port, table_name))
+        self.Session = sessionmaker(bind=inner_engine)
+        self.session = self.Session()
+
+    def get_all(self):
+        result = self.session.query(self.Image.ID, self.Image.image_url, self.Image.caption).all()
+        print(result)
+
+    def findById(self, ID):
+        result = self.session.query(self.Image.name, self.Image.image_url, self.Image.caption).filter(
+            self.Image.ID == ID).one()
+        print(result)
+
+    def updateImageURL(self, ID, new_URL):
+        try:
+            statement = select(self.Image).where(self.Image.ID == ID)
+            retrievedImage = self.session.scalars(statement).one()
+            retrievedImage.image_url = new_URL
+            self.session.commit()
+        except Exception as e:
+            print(e)
+
+    def delete_image(self, ID):
+        statement = self.session.query(self.Image).filter(self.Image.ID == ID).delete()
+        self.session.commit()
+
+    def insertNoCaption(self, image_url):
+        self.session.add(self.Image(image_url=image_url))
+        self.session.commit()
+
+    def insertWithCaption(self, image_url, caption):
+        self.session.add(self.Image(image_url=image_url, caption=caption))
+        self.session.commit()

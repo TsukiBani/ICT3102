eca077a14ab38f22e7b88da705e5d1991df436a6
Joshua Leong <90120658+JoshuaLSE@users.noreply.github.com>
2022-11-14 22:51:08 +0800

    Formatting

--- app.py
+++ app.py
@@ -1,13 +1,6 @@
-from distutils.log import debug
 from website import create_app
 
-from typing import List, Dict
-from flask import Flask
-import mysql.connector
-import json
-
 app = create_app()
-
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
